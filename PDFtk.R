93db5c6e4725a20842fcb5b0cbe5be29aab0bb28
Maria Ascolese <99254839+maria-ascolese@users.noreply.github.com>
2024-08-13 19:48:27 +0200

    Update PDFtk.R

--- PDFtk.R
+++ PDFtk.R
@@ -1,23 +1,23 @@
-# Make sure to install PDF Toolkit before running this script!
+# Make sure to install PDF Toolkit (pdftk) before running this script!
 
-# Define a path for convenience's sake
+# Define a path for convenience
 # dir.create("pdf_files")
 output_path <- file.path("pdf_files", "output.pdf")
 
-# Render the unprotected pdf
+# Render the unprotected PDF
 rmarkdown::render('input_pdftk.Rmd', output_file = output_path) 
 
 # Define a password
 password <- "password"
 
-# Define a path for protected files. Use the same path as before to overwrite
+# Define a path for the protected file. Use the same path as before to overwrite
 protected_output_path <- file.path("pdf_files", "protected_output.pdf")
 
-# Write the password-protected PDF 
+# Create the password-protected PDF 
 system2("pdftk", args = c(output_path, "output", protected_output_path, "user_pw", password))
 
 
 # If you're going to loop the rendering to create password-protected personal files
-# you're going to need to remove all temporary files
+# you'll need to remove all temporary files
 file_name <- "output"
 file.remove(paste0(file_name, c(".log", ".tex", ".md")))
